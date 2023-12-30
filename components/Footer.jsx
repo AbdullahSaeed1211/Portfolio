e2911b5aff3fdde8062a7d8b89d2ee451e44b4d4
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2023-12-30 17:02:22 +0530

    footer change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,35 +10,35 @@ const Footer = () => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
-  const fadeInAnimationsVariants = {
-    initial: { opacity: 0, y: 100 },
-    animate: (index) => ({
-      opacity: 1,
-      y: 0,
+  // const fadeInAnimationsVariants = {
+  //   initial: { opacity: 0, y: 100 },
+  //   animate: (index) => ({
+  //     opacity: 1,
+  //     y: 0,
 
-      transition: {
-        delay: 0.05 * index,
-        duration: 0.5,
-        type: "spring",
-        bounce: 0.25,
-      },
-    }),
-  };
+  //     transition: {
+  //       delay: 0.05 * index,
+  //       duration: 0.5,
+  //       type: "spring",
+  //       bounce: 0.25,
+  //     },
+  //   }),
+  // };
 
-  const fadeInAnimationsVariantsX = {
-    initial: { opacity: 0, x: 0 },
-    animate: (index) => ({
-      opacity: 1,
-      x: 1,
+  // const fadeInAnimationsVariantsX = {
+  //   initial: { opacity: 0, x: 0 },
+  //   animate: (index) => ({
+  //     opacity: 1,
+  //     x: 1,
 
-      transition: {
-        delay: 0.05 * index,
-        duration: 0.75,
-        type: "spring",
-        bounce: 0.25,
-      },
-    }),
-  };
+  //     transition: {
+  //       delay: 0.05 * index,
+  //       duration: 0.75,
+  //       type: "spring",
+  //       bounce: 0.25,
+  //     },
+  //   }),
+  // };
 
   return (
     <footer className="bg-transparent w-full text-gray-600 py-10">
@@ -51,20 +51,28 @@ const Footer = () => {
           <span className="text-lg flex font-semibold mb-4">Quick Links</span>
           <ul>
             {navLinks.map((link, index) => (
-              <motion.li
+              // <motion.li
+              //   key={index}
+              //   variants={fadeInAnimationsVariants}
+              //   initial="initial"
+              //   whileInView="animate"
+              //   ref={ref}
+              //   animate={controls}
+              //   viewport={{ once: true }}
+              //   custom={index}
+              //   className="mb-2">
+              //   <Link href={link.href}>
+              //     <span className="text-sm">{link.text}</span>
+              //   </Link>
+              // </motion.li>
+              <li
                 key={index}
-                variants={fadeInAnimationsVariants}
-                initial="initial"
-                whileInView="animate"
-                ref={ref}
-                animate={controls}
-                viewport={{ once: true }}
-                custom={index}
+                
                 className="mb-2">
                 <Link href={link.href}>
                   <span className="text-sm">{link.text}</span>
                 </Link>
-              </motion.li>
+              </li>
             ))}
           </ul>
         </div>
@@ -72,20 +80,27 @@ const Footer = () => {
           <span className="text-lg flex font-semibold mb-4">Projects</span>
           <ul>
             {ProjectCardList.map((link, index) => (
-              <motion.li
+              // <motion.li
+              //   key={index}
+              //   variants={fadeInAnimationsVariants}
+              //   initial="initial"
+              //   whileInView="animate"
+              //   ref={ref}
+              //   animate={controls}
+              //   viewport={{ once: true }}
+              //   custom={index}
+              //   className="mb-2">
+              //   <Link href={link.projectLink}>
+              //     <span className="text-sm">{link.title}</span>
+              //   </Link>
+              // </motion.li>
+              <li
                 key={index}
-                variants={fadeInAnimationsVariants}
-                initial="initial"
-                whileInView="animate"
-                ref={ref}
-                animate={controls}
-                viewport={{ once: true }}
-                custom={index}
                 className="mb-2">
                 <Link href={link.projectLink}>
                   <span className="text-sm">{link.title}</span>
                 </Link>
-              </motion.li>
+              </li>
             ))}
           </ul>
         </div>
@@ -95,21 +110,29 @@ const Footer = () => {
           </span>
           <div className="flex justify-start md:justify-end gap-6 mt-2">
             {footerLinks.map((link, index) => (
-              <motion.a
+              // <motion.a
+              //   key={index}
+              //   variants={fadeInAnimationsVariantsX}
+              //   initial="initial"
+              //   whileInView="animate"
+              //   ref={ref}
+              //   animate={controls}
+              //   viewport={{ once: true }}
+              //   custom={index}
+              //   href={link.href}
+              //   target="_blank"
+              //   rel="noopener noreferrer"
+              //   className="text-sm">
+              //   {link.icon}
+              // </motion.a>
+              <a
                 key={index}
-                variants={fadeInAnimationsVariantsX}
-                initial="initial"
-                whileInView="animate"
-                ref={ref}
-                animate={controls}
-                viewport={{ once: true }}
-                custom={index}
                 href={link.href}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-sm">
                 {link.icon}
-              </motion.a>
+              </a>
             ))}
           </div>
         </div>
